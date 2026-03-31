9515f4754ca40d50f02ea3bef213fe7d8d176c72
Michael Schock <mschoc3@wgu.edu>
2026-03-31 12:01:05 -0400

    completed EDA and Added arguments for Basic Cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -4,6 +4,8 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 """
 import argparse
 import logging
+import string
+
 import wandb
 import pandas as pd
 
@@ -57,44 +59,44 @@ if __name__ == "__main__":
   
     parser.add_argument(
         "--input_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type = str,    ## INSERT TYPE HERE: str, float or int,
+        help = "The initial artifact to be cleaned",## INSERT DESCRIPTION HERE,
+        required = True,
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type = str,## INSERT TYPE HERE: str, float or int,
+        help = "The output artifact of cleaned data",## INSERT DESCRIPTION HERE,
+        required = True,
     )
 
     parser.add_argument(
         "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type = str,## INSERT TYPE HERE: str, float or int,
+        help = "Type of the output dataset",## INSERT DESCRIPTION HERE,
+        required = True,
     )
 
     parser.add_argument(
         "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type = str,## INSERT TYPE HERE: str, float or int,
+        help = "Description of the output dataset",## INSERT DESCRIPTION HERE,
+        required = True,
     )
 
     parser.add_argument(
         "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type = float,## INSERT TYPE HERE: str, float or int,
+        help = "Minimum house price to be considered",## INSERT DESCRIPTION HERE,
+        required = True,
     )
 
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type = float,## INSERT TYPE HERE: str, float or int,
+        help = "Maximum house price to be considered",## INSERT DESCRIPTION HERE,
+        required = True,
     )
 
 
